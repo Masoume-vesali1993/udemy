56e5ed425c463446717e2f8980b59f96f805588d
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-19 03:21:45 +0430

    Use
    React in show text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Hi im a react App</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //       <h1>Hi im a react App</h1>
+  //   </div>
+  // );
+  return React.createElement('div' , null, 'h1', 'Hi, Im a React App!!!');
 }
 
 export default App;
