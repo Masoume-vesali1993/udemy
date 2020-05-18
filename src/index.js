48019aca9b8d8199aba4da9e4de3980dcecde693
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-19 03:21:22 +0430

    update
    App

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Test</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
