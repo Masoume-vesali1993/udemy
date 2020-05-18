3950d5f2777da4a626e445aa3b8d5dac346e2f61
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-19 03:30:19 +0430

    show
    jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //       <h1>Hi im a react App</h1>
-  //   </div>
-  // );
-  return React.createElement('div' , null, React.createElement('h1',{className: 'App'}, 'Does this work now?'));
+  return (
+    <div className="App">
+        <h1>Hi im a react App</h1>
+    </div>
+  );
+  // return React.createElement('div' , null, React.createElement('h1',{className: 'App'}, 'Does this work now?'));
 }
 
 export default App;
