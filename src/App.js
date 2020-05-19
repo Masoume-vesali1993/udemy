e1bc8445bd47734e6857d61c1e8b059093475034
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-20 00:07:39 +0430

    import
    component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
+import person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
         <h1>Hi im a react App</h1>
+        <Person />
     </div>
   );
   // return React.createElement('div' , null, React.createElement('h1',{className: 'App'}, 'Does this work now?'));
