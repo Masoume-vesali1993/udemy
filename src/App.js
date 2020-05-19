2d8e9888d4dc0bc95f5cdef227ece5ffe13d0023
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-20 00:42:51 +0430

    Use
    Props in react

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
         <h1>Hi im a react App</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
     </div>
   );
   // return React.createElement('div' , null, React.createElement('h1',{className: 'App'}, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I am a person and I am {Math.random() *30} yers old!</p>
+const person = (props) => {
+    return <p>I am a person {props.name} and I am {props.age} yers old!</p>
 };
 
 export default person;
