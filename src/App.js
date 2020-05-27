b2c3840e11b96d782bc27a7735dcf3f803ce782a
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-28 00:02:54 +0430

    use
    onclick in tag p

--- src/App.js
+++ src/App.js
@@ -65,9 +65,16 @@ class App extends Component {
               <h1>Hi, I'm a React App</h1>
               <p>This is really working.</p>
               <button onClick={this.switchNameHandler}>Switch Name</button>
-              <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}>My Passion: Hiking</Person>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+              <Person
+               click={this.switchNameHandler} name={this.state.persons[0].name} 
+               age={this.state.persons[0].age}
+               click={this.switchNameHandler}>My Passion: Hiking</Person>
+              <Person
+               name={this.state.persons[1].name} 
+               age={this.state.persons[1].age}/>
+              <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}/>
           </div>
       );
   }
