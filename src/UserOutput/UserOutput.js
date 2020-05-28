32baeb426fddf57ff5799aaa9e69c8b253d7edf0
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-29 01:39:40 +0430

    style and classname
    in component

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './UserOutput.css';
+
 const userOutput = (props) => {
     return (
-        <div>
+        <div className="UserOutput">
             <p>UserName: {props.userName} </p>
             <p>I hope I ll be overwritten!</p>
         </div>
